cfa9c4f0863c199b72f353630ff87c8055187a45
AMR <amrhoss96@gmail.com>
2023-03-26 21:08:12 +0200

    editting main printf file

--- _printf.c
+++ _printf.c
@@ -9,10 +9,8 @@
   */
 int _printf(const char *format, ...)
 {
-	int count;
-	int total = 0;
+	int count, total = 0, flag = 0;
 	va_list args;
-	int flag = 0;
 
 	if (format == NULL)
 		return (0);
@@ -36,27 +34,6 @@ int _printf(const char *format, ...)
 				case '%':
 					_putchar('%');
 					total += 1;
-					break;
-				case 'd':
-					total += _print_int((long)(va_arg(args, int)));
-					break;
-				case 'i':
-					total += _print_int((long)(va_arg(args, int)));
-					break;
-				case 'b':
-					total += to_Binary(va_arg(args, int));
-					break;
-				case 'u':
-					total += _print_int(va_arg(args, unsigned int));
-					break;
-				case 'o':
-					total += to_Octal(va_arg(args, int));
-					break;
-				case 'x':
-					total += to_Hexa(va_arg(args, int));
-					break;
-				case 'X':
-					total += to_Hexa(va_arg(args, int));
 					break;
 				default:
 					_putchar('%');
